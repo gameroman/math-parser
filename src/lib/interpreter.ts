2725d2e12db93a30d2ba057d3c38fb8e68ecc3bf
Roman <dev@rman.dev>
2026-03-03 21:04:40 +0000

    Update interpreter.ts

--- src/lib/interpreter.ts
+++ src/lib/interpreter.ts
@@ -47,6 +47,7 @@ export type Value = {
 function simplify(v: Value): Value {
   if (v.d === 0n) throw new InterpreterError("Division by zero");
   if (v.n === 0n) return { n: 0n, d: 1n, c: v.c };
+  if (v.d === 1n) return v;
   const common = gcd(v.n, v.d);
   const sign = v.d < 0n ? -1n : 1n;
   return { n: (v.n / common) * sign, d: (v.d / common) * sign, c: v.c };
