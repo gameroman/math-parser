389619c25824559f662e00945754107dbef4a940
Roman <dev@rman.dev>
2026-03-04 11:38:02 +0000

    update

--- src/lib/utils/mod.ts
+++ src/lib/utils/mod.ts
@@ -5,10 +5,10 @@ import type { Value } from "./types";
  * Calculates remainder of division.
  */
 export function mod(a: Value, b: Value): Value {
-  const d = (a.d * b.d) / gcd(a.d, a.d);
+  const d = (a.d * b.d) / gcd(a.d, b.d);
 
   const n1 = a.n * (d / a.d);
-  const n2 = b.n * (d / a.d);
+  const n2 = b.n * (d / b.d);
 
   const n = ((n1 % n2) + n2) % n2;
 
